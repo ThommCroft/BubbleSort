8020b483220bec06d1773d0009a80868e980e496
ThommCroft <thomas.wolstencroft@outlook.com>
2022-07-19 17:18:28 +0100

    Modified two unit tests to sort both negative and positive numbers and added the tests to a happy path category.

--- BubbleSortLibrary/BubbleSortLibraryTests/BubbleSortTests.cs
+++ BubbleSortLibrary/BubbleSortLibraryTests/BubbleSortTests.cs
@@ -1,26 +1,25 @@
 namespace BubbleSortLibraryTests
 {
-    public class Tests
+    [TestFixture]
+    public class BubbleSortTests
     {
         [Test]
-        public void GivenAnArrayOfNumbers_BubbleSort_returnThemFromLowestToHighest()
-        {
-            int[] subject = new int[] { 10, 4, 5, 3, 9, 7, 1, 6, 2, 8 };
-            int[] expectedResult = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            var sorting = new BubbleSorting();
-
-            Assert.That(sorting.BubbleSort(subject), Is.EqualTo(expectedResult));
-        }
-
-        [Test]
+        [Category("Happy Path")]
         public void GivenAnEmptyArrays_BubbleSort_returnEmptyArray()
         {
             int[] subject = new int[] { };
-            int[] expectedResult = new int[] { };
 
             var sorting = new BubbleSorting();
+            Assert.That(sorting.BubbleSort(subject), Is.Empty);
+        }
 
+        [TestCase(new int[] { -1, -6, -23, -50 }, new int[] { -50, -23, -6, -1 })]
+        [TestCase(new int[] { 70, 3, 40, 0, -60, -7, -3}, new int[] { -60, -7, -3, 0, 3, 40, 70 })]
+        [TestCase(new int[] { 10, 4, 5, 3, 9, 7, 1, 6, 2, 8 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+        [Category("Happy Path")]
+        public void GivenAnArrayOfNumbers_BubbleSort_returnThemFromLowestToHighest(int[] subject, int[] expectedResult)
+        {
+            var sorting = new BubbleSorting();
             Assert.That(sorting.BubbleSort(subject), Is.EqualTo(expectedResult));
         }
     }
